64072917216d41a932e4935b0d44677f1b20d328
Generalkidd <generalheed@rocketmail.com>
2016-07-11 23:59:47 -0400

    Added speech support
    Speech commands are now supported and work quite well. Now they just
    need to be linked to the state machines for the animations and pokemon's
    actions.

--- Pokemon Battles/Assets/Scripts/BattleCommands.cs
+++ Pokemon Battles/Assets/Scripts/BattleCommands.cs
@@ -1,25 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BattleCommands : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+public class BattleCommands : MonoBehaviour
+{
+    Vector3 originalPosition;
+    // Use this for initialization
+    void Start ()
+    {
+        originalPosition = this.transform.localPosition;
+    }
     
     void OnAttack()
     {
-
+        OnReset();   
     }
 
     void OnDodge()
     {
-
+        OnReset();
     }
 
     void OnEvade()
     {
+        OnReset();
+    }
 
+    void OnReset()
+    {
+        // Put the sphere back into its original local position.
+        this.transform.localPosition = new Vector3(100, 100, 100);
     }
 }
